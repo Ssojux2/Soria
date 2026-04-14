e75038930d203c72a820847fbd7a62da6070e749
Ssojux2 <sjs0827@naver.com>
2026-04-14 13:53:40 +0900

    Initial Commit

---
+++ Soria/SoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoriaApp.swift
+//  Soria
+//
+//  Created by Junseop So on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoriaTests/SoriaTests.swift
@@ -0,0 +1,19 @@
+//
+//  SoriaTests.swift
+//  SoriaTests
+//
+//  Created by Junseop So on 4/14/26.
+//
+
+import Testing
+@testable import Soria
+
+struct SoriaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
